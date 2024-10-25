58a855c29d967a076af183da52e680d8bb1bfe35
samuel161415 <samuelnegalign1@gmail.com>
2024-10-25 08:42:07 +0300

    sami.fix: adding the tree view for the list

---
+++ src/plugins/syncfusion.js
@@ -0,0 +1,52 @@
+// // plugins/syncfusion.js
+// import Vue from 'vue'
+// import {
+//   BookmarkDialog,
+//   BordersAndShadingDialog,
+//   BulletsAndNumberingDialog,
+//   CellOptionsDialog,
+//   ContextMenu,
+//   DocumentEditorPlugin,
+//   Editor,
+//   EditorHistory,
+//   FontDialog,
+//   HyperlinkDialog,
+//   ImageResizer,
+//   ListDialog,
+//   OptionsPane,
+//   PageSetupDialog,
+//   ParagraphDialog,
+//   Print,
+//   Search,
+//   Selection,
+//   SfdtExport,
+//   StyleDialog,
+//   StylesDialog,
+//   TableDialog,
+//   TableOfContentsDialog,
+//   TableOptionsDialog,
+//   TablePropertiesDialog,
+//   TextExport,
+//   WordExport,
+// } from '@syncfusion/ej2-vue-documenteditor'
+// import { registerLicense } from '@syncfusion/ej2-base'
+
+// // // Register your Syncfusion license key
+// registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5DQmFIYVF2R2dJfFRxcV9HZkwxOX1dQl9nSH5RdERlWHhbdXdVQmk=')
+
+// Vue.use(DocumentEditorPlugin)
+import { registerLicense } from '@syncfusion/ej2-base';
+
+export default defineNuxtPlugin(() => {
+    registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5DQmFIYVF2R2dJfFRxcV9HZkwxOX1dQl9nSH5RdERlWHhbdXdVQmk=');
+});
+
+
+// import Vue from 'vue'
+// import { TreeViewPlugin } from '@syncfusion/ej2-vue-navigations';
+// import { registerLicense } from '@syncfusion/ej2-base';
+
+// // export default defineNuxtPlugin(() => {
+//     registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5DQmFIYVF2R2dJfFRxcV9HZkwxOX1dQl9nSH5RdERlWHhbdXdVQmk=');
+// // });
+// Vue.use(TreeViewPlugin)
